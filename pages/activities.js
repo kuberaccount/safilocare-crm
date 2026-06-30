46dd9325f285f8c20b31c26353886bc3addc010b
kuberaccount <kuberaccount@safilocare.com>
2026-06-30 15:15:03 +0530

    Update activities.js

--- pages/activities.js
+++ pages/activities.js
@@ -5,7 +5,7 @@ import Modal from "../components/Modal";
 import toast from "react-hot-toast";
 import { exportToCSV, fmtDateForExport } from "../lib/export";
 
-const TYPES = ["Email", "Call", "Meeting", "Note"];
+const TYPES = ["WhatsApp", "Call", "Meeting", "Note"];
 const TYPE_ICONS = {
   Email: { icon: "M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z", color: "bg-blue-100 text-blue-600" },
   Call:  { icon: "M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z", color: "bg-green-100 text-green-600" },
